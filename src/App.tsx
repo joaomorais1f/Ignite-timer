009ff75b573eed822850862991a856506ce15189
joaomorais1f <ifsp.joaov@gmail.com>
2022-10-31 19:09:22 -0300

    fix countdown bug and list cycles on history page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { Router } from './Route'
 import { GlobalStyle } from './styles/global'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
